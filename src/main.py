7e0715e3cb5250b5b8183170ea4b52bf8e1a0632
Josh <joshmstout@gmail.com>
2016-12-07 19:16:01 -0600

    main.py update for potential

--- src/main.py
+++ src/main.py
@@ -3,20 +3,25 @@ import matplotlib.pyplot as plt
 import time
 import nlist as n
 from initSi import PureSi
+from SWPo import *
 
-lat = 5.431 #silicon lattice constant
+lat = 4.6 #silicon lattice constant
+sigmasi = 2.0951
+
 x = PureSi(3,3,3,lat)
 
+#print(x)
 nx = np.array([-1.5,1.5])
 ny = np.array([-1.5,1.5])
 nz = np.array([-1.5,1.5])
+Lb = [lat*(nx[1]-nx[0]),lat*(ny[1]-ny[0]),lat*(nz[1]-nz[0])] # box size, Stillinger-Weber compressed domain
 
 #simulation bounds for binning
 bx = nx*lat
 by = ny*lat
 bz = nz*lat
 
-rc = 4 #cutoff radius = minimum cell size (for the neighborlist's cutoff radius) 
+rc = sigmasi*1.8 #cutoff radius = minimum cell size (for the neighborlist's cutoff radius) 
 #rc = 1.8*2.0951  #this is the potential's cutoff radius
 
 nl2,np2,nlf2,npf2 = n.nlist2(bx,by,bz,rc,x)
@@ -29,4 +34,4 @@ nl3,nlp3 = n.nlist3(nlf2,npf2)
 np.savetxt('Full3BodyList.txt',nl3,fmt='%1.0i')
 np.savetxt('Full3BodyListPointers.txt',nlp3,fmt='%1.0i')
 
-
+SWPotAll(nl2,np2,nl3,x,Lb)
